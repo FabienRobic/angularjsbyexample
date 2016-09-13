299e84e25c8875cc534bf09de30116c03cc5b3f0
FabienRbc <robicfabien@gmail.com>
2016-09-13 11:05:48 +0200

    Modification de l'architecture. Suppression du dossier 7MinWorkout

--- 7MinWorkout/trainer/app/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-angular.module('app', ['ngRoute', 'ngSanitize', '7MinWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate', 'ngMessages', 'ngResource'])
-
-angular.module('7MinWorkout', [])
-angular.module('WorkoutBuilder', [])
