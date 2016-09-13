8b3e6dcb62351f76705b4d7b0ebd98d8ffc5688b
FabienRbc <robicfabien@gmail.com>
2016-09-13 10:49:35 +0200

    Implementation of route error redirection

--- 7MinWorkout/trainer/app/js/config.js
+++ 7MinWorkout/trainer/app/js/config.js
@@ -41,6 +41,7 @@ angular.module('app').config(function ($routeProvider, $sceDelegateProvider, Wor
     leftNav: 'partials/workoutbuilder/left-nav-exercises.html',
     controller: 'WorkoutDetailController',
     topNav: 'partials/workoutbuilder/top-nav.html',
+    routeErrorMessage: 'Could not load the specific workout!',
     resolve: {
       selectedWorkout: ['WorkoutBuilderService', '$route', '$location', function (WorkoutBuilderService, $route, $location) {
         return WorkoutBuilderService.startBuilding($route.current.params.id)
--- 7MinWorkout/trainer/app/js/root.js
+++ 7MinWorkout/trainer/app/js/root.js
@@ -23,5 +23,12 @@ angular.module('app')
 
     $scope.$on('$routeChangeSuccess', function (e, current, previous) {
       $scope.currentRoute = current
+      $scope.routeHasError = false
+    })
+    $scope.$on('$routeChangeError', function (e, current, previous, error) {
+      if (error.status === 404 && current.originalPath === '/builder/workouts/:id') {
+        $scope.routeHasError = true
+        $scope.routeError = current.routeErrorMessage
+      }
     })
   }])
